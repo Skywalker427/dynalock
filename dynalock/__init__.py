37092c0b81e2b1a5fe891276d7000b98bc94b49d
Hamzah Bawah <bhamza123@gmail.com>
2024-04-01 16:57:59 +0100

    Bump version: 0.1.0 → 0.1.1

--- dynalock/__init__.py
+++ dynalock/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Hamzah Bawah"""
 __email__ = 'bhamza123@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .dynalock import DynaLock
 from .exceptions import LockAlreadyAcquiredError, LockAcquisitionError, LockReleaseError
